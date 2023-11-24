58c311ca2cf76b4d94db352d945dd113619b86c3
Olegbotandniger <122714019+Olegbotandniger@users.noreply.github.com>
2023-11-24 16:57:48 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="css/style.css">
+<title>Заметки</title>
+</head>
+<body>
+<h1>Добавить Заметку</h1>
+<form method="post" action="display_notes.php">
+<label for="name">Название:</label>
+<input type="text" id="name" name="name" required><br>
+<label for="category">Категория:</label>
+<select id="category" name="category" required>
+<option value="Книга">Книга</option>
+<option value="Мультфильм">Мультфильм</option>
+<option value="Фильм">Фильм</option>
+</select><br>
+<input type="submit" value="Добавить">
+</form>
+<h2>Заметки</h2>
+<table border="1">
+<tr>
+<th>Название</th>
+<th>Категория</th>
+</tr>
+<?php include 'display_notes.php'; ?>
+</table>
+</body>
+</html>
